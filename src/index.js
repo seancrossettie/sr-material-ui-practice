85ff29fdcb577ad8249af6e472ec3c350ae7d9f4
Sean <jb@JBs-MacBook-Pro.local>
2021-04-28 18:26:20 -0500

    added basic header

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import theme from './styles/themes';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.Fragment>
+    <CssBaseline />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
